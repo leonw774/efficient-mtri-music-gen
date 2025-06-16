5bb63fc66ae3798bca058db43c41c78c4030be85
leonw774 <leonw774@gmail.com>
2025-06-16 21:28:35 +0800

    remove tqdm grad accu step

--- train.py
+++ train.py
@@ -770,13 +770,7 @@ def main():
                 0.0
                 for _ in train_output_attr_name
             ])
-            tqdm_range_grad_accu_steps = tqdm(
-                range(gradient_accumulation_steps),
-                disable=(gradient_accumulation_steps<=64),
-                ncols=0,
-                leave=None
-            )
-            for ga_step in tqdm_range_grad_accu_steps:
+            for ga_step in range(gradient_accumulation_steps):
                 # if use parallel and gradient accumulation step isnt the last
                 # then we can use no sync
                 if (args.use_parallel
